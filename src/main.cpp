edeba0b1370e190e06bcbdba16b660b86519d220
Danylo Sahaidak <danylo.sahaidak@gmail.com>
2021-01-19 02:09:36 +0200

    Argument parsing - done, saving results to json - done.

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,53 @@
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/asio/thread_pool.hpp>
+#include <boost/program_options.hpp>
 
 #include <thread>
 #include <mutex>
 
-#include "inc/read_files.h"
-#include "inc/process_files.h"
+#include "read_files.h"
+#include "process_files.h"
 #include "time_counting.h"
+#include "json_saver.h"
 
-int main() {
+int main(int argc, char **argv) {
+    //                           <------------ program options ------------>
+    namespace po = boost::program_options;
 
-//    commented code
-    std::string root_path = "/home/sotnyk/sotnyk/"; //some comment
-//    std::string root_path = "../..";
+    std::string root_path;
+
+    po::options_description visible("Supported options");
+    visible.add_options()
+            ("help,h", "Print this help message.")
+            ("save,s",  po::value<std::string>(), "Save results to the file.")
+            ("threads,t", po::value<int>(), "Number of working threads");
+
+    po::options_description hidden("Hidden options");
+    hidden.add_options()
+            ("root_path", po::value(&root_path), "Path to the root folder.");
+
+    po::positional_options_description p;
+    p.add("root_path", -1);
+
+    po::options_description all("All options");
+    all.add(visible).add(hidden);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(all).positional(p).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << "Usage:\n  small_code_analyser <path> [-s savefile]\n" << visible << std::endl;
+        return EXIT_SUCCESS;
+    }
+
+
+
     std::chrono::high_resolution_clock::time_point start_time, indexing_time, finish_time;
     start_time = get_current_time_fenced();
 
+    //                        <------------ getting source files ------------>
     std::vector<std::string> files = get_source_files(root_path);
 
     std::cout << files.size() << std::endl;
@@ -31,7 +62,17 @@ int main() {
             {"all", 0}
     };
 
-    boost::asio::thread_pool pool(1);
+    int working_threads;
+    if (vm.count("threads")) {
+        working_threads = vm["threads"].as<int>();
+    }
+    else {
+        working_threads = std::thread::hardware_concurrency();
+    }
+
+    std::cout << working_threads << std::endl;
+
+    boost::asio::thread_pool pool(working_threads);
     std::mutex m;
     for (auto & file: files) {
         boost::asio::post(pool, boost::bind(count_lines, file, std::ref(m), std::ref(results)));
@@ -45,20 +86,33 @@ int main() {
 
 
 //    std::vector<std::thread> threads;
-//
 //    std::mutex m;
-//
 //    for (auto & file: files) {
 //        threads.emplace_back(count_lines, file, std::ref(m), std::ref(results));
 //    }
 //    for(auto& thread : threads) {
 //        thread.join();
 //    }
+//    for (auto &value:results) {
+//        std::cout << value.first << " " << value.second << std::endl;
+//    }
 
     finish_time = get_current_time_fenced();
     std::cout << to_us(indexing_time - start_time)  <<  std::endl;
     std::cout << to_us(finish_time - indexing_time)  <<  std::endl;
     std::cout << to_us(finish_time - start_time)  <<  std::endl;
+
+    std::map<std::string, double> times = {
+            {"parsing directories", to_us(indexing_time - start_time)},
+            {"parsing files", to_us(finish_time - indexing_time)},
+            {"full program", to_us(finish_time - start_time)}
+    };
+
+    if (vm.count("save")) {
+        std::cout << "Saving results to: " << vm["save"].as<std::string>() << std::endl;
+        save_json(vm["save"].as<std::string>(),root_path, working_threads, files.size(), results, times);
+    }
+
 
     return 0;
 }
--- src/src/read_files.cpp
+++ src/src/read_files.cpp
@@ -6,7 +6,7 @@
 
 std::vector<std::string> get_source_files(std::string & root_path, std::set<std::string> extension_masks) {
     std::vector<std::string> files;
-    for (const auto & entry : boost::filesystem::recursive_directory_iterator(root_path)) {
+    for (const auto & entry : boost::filesystem::recursive_directory_iterator(root_path,boost::filesystem::directory_options::skip_permission_denied)) {
         if ( extension_masks.count(boost::filesystem::extension(entry.path()))) {
             files.push_back(entry.path().string());
         }
